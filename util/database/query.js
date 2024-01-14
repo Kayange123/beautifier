24706b7ba4f040dda765f06f0342fa376248da6b
Ayubu Kayange <kayangejr3@gmail.com>
2024-01-14 21:17:00 +0300

    data fetcher

---
+++ util/database/query.js
@@ -0,0 +1,20 @@
+import { sanityClient } from "../../lib/sanity";
+
+export const getProducts = async () => {
+  const query = `*[_type == "product"]{
+    _id,
+    productTitle,
+    slug,
+    mainImage { asset->{ url }},
+    bgImage { asset->{ url }},
+    description,
+    price,
+    productType,
+    categories[] ->{
+      title,
+      mainImage { asset->{ url }},
+    }
+  }`;
+  const posts = await sanityClient.fetch(query);
+  return posts;
+};
