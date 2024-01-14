bdfcad724829a2d64904995630a17ba411adf218
Ayubu Kayange <kayangejr3@gmail.com>
2024-01-14 21:14:22 +0300

    app assets

---
+++ assets/index.js
@@ -0,0 +1,5 @@
+export { default as screeOneImage } from "./images/screen1.png";
+export { default as screenTwoImage } from "./images/screen2.jpg";
+export { default as screenThreeImage } from "./images/screen3.jpeg";
+export { default as emptyCart } from "./images/emptyCart.png";
+export { default as brand } from "./images/brand.png";
